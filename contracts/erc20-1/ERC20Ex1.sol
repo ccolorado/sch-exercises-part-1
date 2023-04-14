5fccb28e20f8e7e45cc979bbc67fe689575db101
Carlos Colorado <carlos.colorado@gmail.com>
2023-04-14 00:11:02 -0600

    Solves exercise 1

---
+++ contracts/erc20-1/ERC20Ex1.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+pragma experimental ABIEncoderV2;
+
+import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract ERC20Ex1 is ERC20PresetMinterPauser, Ownable {
+
+  constructor(string memory name, string memory symbol) ERC20PresetMinterPauser(name, symbol) Ownable() {
+  }
+
+  function mint(address _to, uint256 _amount) public override {
+    super.mint(_to, _amount);
+  }
+
+}
+
--- test/erc20-1/tests.js
+++ test/erc20-1/tests.js
@@ -2,8 +2,9 @@ const { ethers } = require('hardhat');
 const { expect } = require('chai');
 
 describe('ERC20 Tokens Exercise 1', function () {
-    
+
     let deployer, user1, user2, user3;
+    let Token, token;
 
     // Constants 
     const DEPLOYER_MINT = ethers.utils.parseEther('100000');
@@ -13,27 +14,50 @@ describe('ERC20 Tokens Exercise 1', function () {
 
     before(async function () {
         /** Deployment and minting tests */
-        
-        [deployer, user1, user2, user3] = await ethers.getSigners();
 
-        // TODO: Contract deployment
-        
-        // TODO: Minting
-        
-        // TODO: Check Minting
+        [deployer, user1, user2, user3] = await ethers.getSigners();
+        // Contract deployment
+        Token = await ethers.getContractFactory('ERC20Ex1');
+        token = await Token.deploy('Exercise1', 'ex1');
+
+        expect(await token.owner()).to.equal(deployer.address);
+
+        // Minting
+        await token.mint(deployer.address, DEPLOYER_MINT);
+
+        // Check Minting
+        expect(await token.balanceOf(deployer.address)).to.equal(DEPLOYER_MINT);
 
     });
 
     it('Transfer tests', async function () {
-        /** Transfers Tests */
+      /** Transfers Tests */
 
-        // TODO: First transfer
+      // First transfer
+      await token.mint(user1.address, USERS_MINT);
+      await token.mint(user2.address, USERS_MINT);
+      await token.mint(user3.address, USERS_MINT);
 
-        // TODO: Approval & Allowance test
+      expect(await token.balanceOf(user1.address)).to.be.equal(USERS_MINT);
+      expect(await token.balanceOf(user2.address)).to.be.equal(USERS_MINT);
+      expect(await token.balanceOf(user3.address)).to.be.equal(USERS_MINT);
 
-        // TODO: Second transfer
+      //  Second transfer
+      await token.connect(user2).transfer(user3.address, FIRST_TRANSFER)
 
-        // TODO: Checking balances after transfer
-        
+      expect(await token.balanceOf(user2.address)).to.be.equal(ethers.utils.parseEther('4900'));
+      expect(await token.balanceOf(user3.address)).to.be.equal(ethers.utils.parseEther('5100'));
+
+      // Approval & Allowance test
+      await token.connect(user3).approve(user1.address, SECOND_TRANSFER)
+
+      expect(await token.allowance(user3.address, user1.address)).to.be.equal(SECOND_TRANSFER);
+
+      await token.connect(user1).transferFrom(user3.address, user1.address, SECOND_TRANSFER);
+
+      // Checking balances after transfer
+      expect(await token.balanceOf(user3.address)).to.be.equal(ethers.utils.parseEther('4100'));
+      expect(await token.balanceOf(user1.address)).to.be.equal(ethers.utils.parseEther('6000'));
+
     });
 });
